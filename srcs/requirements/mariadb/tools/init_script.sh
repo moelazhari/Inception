7a97ef5bc7b1159d772f671524c4cbff77897584
Your Name <you@example.com>
2023-01-13 13:53:19 -0500

    mariadb problem

--- srcs/requirements/mariadb/tools/init_script.sh
+++ srcs/requirements/mariadb/tools/init_script.sh
@@ -2,11 +2,11 @@
 
 service mysql start
 
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $WORDPRESS_DB_NAME";
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD'";
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%'; FLUSH PRIVILEGES";
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES";
 
-killall -9 mysqld;
+killall -9 mysqld
 
 exec "$@";
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -2,10 +2,10 @@
 
 if [[ ! -f /var/www/html/wp-config.php ]]
 then
-    wp config create --path=/var/www/html/ --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER \
-    --dbpass=$WORDPRESS_DB_PASSWORD --allow-root
+    wp config create --path=/var/www/html/ --dbhost=${WORDPRESS_DB_HOST} --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} \
+    --dbpass=${WORDPRESS_DB_PASSWORD} --allow-root
 
-    wp core install --path=/var/www/html --url=${URL} --title=${TITLE} --admin_user=${WORDPRESS_ADMIN} \
+    wp core install --path=/var/www/html --url=${URL} --title=${TITlE} --admin_user=${WORDPRESS_ADMIN} \
     --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --allow-root
 
     wp user create --path=/var/www/html  ${WORDPRESS_USER}  ${WORDPRESS_USER_EMAIL} --user_pass=${WORDPRESS_USER_PASSWORD}  --allow-root
