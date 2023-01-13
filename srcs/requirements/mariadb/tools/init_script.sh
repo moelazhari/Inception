196ff18848cb672ac5cec336feb52c399bdb3828
Your Name <you@example.com>
2023-01-13 14:22:37 -0500

    mariadb problem

--- srcs/requirements/mariadb/tools/init_script.sh
+++ srcs/requirements/mariadb/tools/init_script.sh
@@ -2,11 +2,9 @@
 
 service mysql start
 
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $WORDPRESS_DB_NAME";
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD'";
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%'; FLUSH PRIVILEGES";
-
-killall -9 mysqld
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
 
 exec "$@";
