4e5b3657fec4e688cb2f554dd36d764ab0d34b20
Your Name <you@example.com>
2023-01-14 18:20:16 -0500

    finish

--- makefile
+++ makefile
@@ -4,8 +4,4 @@ all:
 clean:
 	sudo docker-compose -f ./srcs/docker-compose.yml down --rmi all
 
-clean_volumes:
-	sudo rm -rf /home/mazhari/data/db/* 
-	sudo rm -rf /home/mazhari/data/wp/* 
-
 re: clean all
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -2,6 +2,7 @@
 
 if [[ ! -f /var/www/html/wp-config.php ]]
 then
+    sleep 5;
     wp config create --path=/var/www/html/ --dbhost=${WORDPRESS_DB_HOST} --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} \
     --dbpass=${WORDPRESS_DB_PASSWORD} --allow-root
 
@@ -9,6 +10,7 @@ then
     --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --allow-root
 
     wp user create --path=/var/www/html  ${WORDPRESS_USER}  ${WORDPRESS_USER_EMAIL} --user_pass=${WORDPRESS_USER_PASSWORD}  --allow-root
+
 fi
 
 exec "$@"
